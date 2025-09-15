5d8c271acde2347adf55fafa62f382b0fc20663f
hamzakhledmalk2019-eng <hamzakhledmalk2019@gmail.com>
2025-09-15 21:15:10 +0300

    Add files via upload

---
+++ modal6.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<title>الموديول الأول</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-georgia.js"></script>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+<style type="text/css">
+.auto-style4 {
+	text-align: left;
+	font-family: "حمدين بولد";
+	font-size: x-large;
+	color: #FFFFFF;
+}
+.auto-style8 {
+	font-family: "حمدين بولد";
+	font-size: large;
+}
+.auto-style10 {
+	color: #A56C07;
+}
+.auto-style11 {
+	font-family: "حمدين بولد";
+font-size: x-large;
+	color:#150517;
+}
+.auto-style12 {
+	font-family: "حمدين بولد";
+	font-size: x-large;
+	color: #FFFFFF;
+	padding-bottom: 0;
+}
+.btn-btn-sm-btn-warning{
+	width:20%;
+	padding:7px 10px;
+	font-size:15px;
+	border:0px;
+	background:#33CCFF;
+	color:white;
+	cursor:pointer;
+	border-radius:3px;
+	outline:none;
+	margin-right:20%;	
+        font-family: "حمدين بولد";
+    	font-size:18px;	
+    	text-align:center;				
+}
+
+
+.btn-btn-sm-btn-warning:hover{
+	background:#E8D5A8;	
+}
+
+ .auto-style50 {
+	font-size: large;
+
+}
+
+
+
+
+
+
+
+
+</style>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="menu_nav" style="width: 432px">
+      <ul style="width: 465px">
+          <li class="active"><a href="index.php"><span class="auto-style50">الرئيسية</span></a></li>
+          <li class="active"><a href="instraction.php"><span class="auto-style50">التعليمات</span></a></li>
+           <li class="active"><a href="home.php"><span class="auto-style50">المحتوى</span></a></li>
+        </ul>      </div>
+       <!-------- home --> 
+
+     <div class="logo">
+<p style="width: 439px" class="auto-style19">&nbsp;</p>
+		  <p style="width: 439px" class="auto-style19">
+		  <a href="index.html" class="auto-style18"><span class="auto-style17">  
+  </span><span class="auto-style16"> 
+		  PHp Skills&nbsp; </span> </a></p>
+
+
+		</div>
+  <div class="clr"></div>
+      <div class="slider">
+        <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="960" height="360" alt="" /><span class="auto-style8"><span class="auto-style10"><span class="auto-style11">
+توظيف بيئة تعلم الكتروني تكيفية قائمة على التعلم المصغر وفقاً لأساليب التعلم لتنمية مهارات البرمجة والتقبل التكنولوجي لدي طلاب المرحلة الثانوية
+     </span></span></span></a><span class="auto-style8"><span class="auto-style10"><a href="#"> 
+	
+		</a></span></span> <a href="#"><img src="images/slide2.jpg" width="960" height="360" alt="" /><span> 
+			<span class="auto-style11">  
+    
+   توظيف بيئة تعلم الكتروني تكيفية قائمة على التعلم المصغر وفقاً لأساليب التعلم لتنمية مهارات البرمجة والتقبل التكنولوجي لدي طلاب المرحلة الثانوية</span></span></span></a><span class="auto-style8"><span class="auto-style10"><a href="#"> 
+
+    </span></span></a> <a href="#"><img src="images/slide3.jpg" width="960" height="360" alt="" /><span> 
+			<span class="auto-style11">توظيف بيئة تعلم الكتروني تكيفية قائمة على التعلم المصغر وفقاً لأساليب التعلم لتنمية مهارات البرمجة والتقبل التكنولوجي لدي طلاب المرحلة الثانوية</span></span></span></a><span class="auto-style8"><span class="auto-style10"><a href="#"> 
+</span></span><span class="auto-style21"> 
+			 </span></a> </div>
+                   <!-------- end --> 
+
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar" style="height: 893px">
+        <div class="article">
+          <div class="clr"></div>
+        </div>
+        <div class="article">
+          <div class="clr"></div>
+          <div class="clr"><br />
+		
+			</div>
+        </div>
+
+       	  <iframe name="I1" src="https://app.nearpod.com/?pin=IX9KF" border="0" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="height:780px; width: 720px; float:left;" class="auto-style14"> 
+
+ </iframe>
+    
+
+   
+   <br /><br />  <br /><br />  <br /><br />
+  <table style="width: 76%; height: 39px">
+		
+			  <tr>
+				  
+				  <td class="auto-style35">
+				  <a class="auto-style18" href="modless2.php"><button class="btn-btn-sm-btn-warning">رجوع</button></a></td>
+			  </tr>
+		  </table>
+
+   </div>
+  
+   </div>
+
+  
+      <div class="sidebar">
+        <div class="searchform">
+          <form id="formsearch" name="formsearch" method="post" action="#">
+            <span>
+            <input name="editbox_search" class="editbox_search" id="editbox_search" maxlength="80" value="Search our ste:" type="text" />
+            </span>
+            <input name="button_search" src="images/search.gif" class="button_search" type="image" />
+          </form>
+        </div>
+        <div class="clr"></div>
+        <div class="gadget">
+          <p class="auto-style12">مصادر تعليمية</p>
+          <div class="clr"></div>
+          
+ <style>
+      #bdrs-social {margin:0px auto;width: 300px;}#bdrs-social .bdbox-social a:link,.bdbox-social a:visited{float:left;width:32px;height:32px;margin:0 5px;padding:0;text-indent:-9999em;box-shadow:3px 3px 3px rgba(0,0,0,0.3);background-color:transparent;background-size:100% auto;background-image:url('http://3.bp.blogspot.com/-iqgS6_ZGsWI/VE1SXfAV_qI/AAAAAAAAcXY/AimDpPHiuSc/s1600/BRS01-sosial.png');background-repeat:no-repeat;}#bdrs-social .bdbox-social a:hover,.bdbox-social a:active {box-shadow:3px 3px 7px #333 INSET;}#bdrs-social .bdbox-social a:first-child{margin-left:3px}#bdrs-social .bdbox-social a:last-child{margin-right:0}#bdrs-social .bdrs-rss{background-position:0 0}#bdrs-social .bdrs-goo{background-position:0 -32px}#bdrs-social .bdrs-fac{background-position:0 -64px}#bdrs-social .bdrs-lin{background-position:0 -96px}#bdrs-social .bdrs-tum{background-position:0 -128px}#bdrs-social .bdrs-twi{background-position:0 -160px}#bdrs-social .bdrs-wor{background-position:0 -192px}
+.auto-style13 {
+	font-family: Arial;
+	font-size: small;
+	font-weight: bold;
+}
+ .auto-style15 {
+	 margin: 247px auto 0 auto;
+	 width: 920px;
+	 padding: 16px 20px;
+	 color: #114044;
+ }
+ .auto-style16 {
+	 font-family: "حمدين بولد";
+	 font-size: xx-large;
+ }
+ .auto-style17 {
+	 font-family: "حمدين بولد";
+	 font-size: xx-large;
+	 color: #21C3E1;
+ }
+ .auto-style18 {
+	text-decoration: none;
+}
+.auto-style19 {
+	text-align: center;
+	direction: rtl;
+}
+ .auto-style20 {
+	 text-align: justify;
+	 direction: rtl;
+	 font-family: Arial;
+	 font-weight: bold;
+ }
+ .auto-style21 {
+	 font-family: "حمدين بولد";
+	 font-size: large;
+	 color: #A56C07;
+	 text-decoration: underline;
+ }
+ .auto-style33 {
+	font-weight: bold;
+	color: #FFFFFE;
+}
+.auto-style34 {
+	color: #FFFFFE;
+	text-decoration: underline;
+}
+ .auto-style35 {
+	 text-align: right;
+ }
+
+ </style>
+    <!-- www.madad2.blogspot.com -->
+    <div id="bdrs-social" style="width: 229px">
+      <div class="bdbox-social" >         
+          
+          
+          <ul >
+                       <li><a class="bdrs-fac" href="رابط صفحة فيس بوك" rel="nofollow" target="_blank" title="فيس بوك">
+          فيس بوك
+        </a><br /><br />
+             </li>
+            <li> <a class="bdrs-twi" href="رابط تويتر" rel="nofollow" target="_blank" title="تويتر">
+          تويتر
+        </a><br /><br />
+
+             </li>
+            <li><a class="bdrs-lin" href="رابط ينكدين" rel="nofollow" target="_blank" title="LinkedIn">
+          ينكدين
+        </a><br /><br />
+             </li>
+            <li>   <a class="bdrs-tum" href="رابط مدونة تلنبر" rel="nofollow" target="_blank" title="Tumblr">
+          نعرفكم
+        </a><br /><br />
+              </li>
+            <li>  <a class="bdrs-rss" href="رابط التغذية لمدونتك" rel="external" target="_self" title="RSS Feed">
+          RSS Feed
+        </a><br /><br />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="fbg">
+    <div class="auto-style15">
+
+
+
+ <div class="col c1">
+          <p class="auto-style4">معرض الصور</p>
+     <a href="#"><img src="images/gal1.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal2.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal3.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a> 
+ </div>
+      <div class="col c2" style="height: 211px">
+          <p class="auto-style4">نظرة للخدمات</p>
+        <p class="auto-style20">إننا نسعى من خلال هذا الجهد أن يكون لبنة في المساعدة على  
+ الإبداع والابتكار، وتوظيف التكنولوجيا الحديثة في مشروعات 
+تطبيقية تخدم العملية التعليمية والبيئة المحيطة وتساعد المتعلم على التكامل المواد الدراسية المختلفة 
+من خلال دمج تكنولوجيا المعلومات والاتصالات في العملية التعليمية وذلك من خلال أحدث النظريات 
+والمداخل التربوية الحديثة كمدخل النظم ومدخل أسلوب حل المشكلات للتعلم من خلال العمل التعاوني 
+ومن خلال مشروعات من خلال اتباع الأسلوب العلمي في التفكير</p>
+		  <p>&nbsp;</p>
+		  <p>&nbsp;</p>
+		  <p>&nbsp;</p>
+      </div>
+<!--  home -->
+         <div class="col c3">
+          <p class="auto-style4">تواصل معنا</p>
+        <p class="auto-style13" style="width: 282px">يمكنك عزيزى الطالب التواصل 
+		مع الباحث من خلال</p>
+        <p class="contact_info"> <span>E-mail:</span>hgtamer31@gmail.com<br />
+          <span>Telephone1:</span> 001007426981<br />
+          <span>Telephone2:</span>+<br />
+          <span>Others:</span> <br />
+          <span class="auto-style33">facebook:</span><a href="https://www.facebook.com/profile.php?id=100004572762800">
+		<span class="auto-style34"></span>01019707757 </a> </p>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize" style="width: 967px">
+      <p class="lf"> جميع الحقوق محفوظة للباحث © للباحث /تامر ابراهيم عبدالفتاح البلتاجى</p>
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+<!--  end-->
+</body>
+</html>
